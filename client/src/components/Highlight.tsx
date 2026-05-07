65b2bd1fb5863c83078bd368fc759d6cae9d7c65
arishalidev <hello@arishali.dev>
2026-05-07 11:26:37 -0500

    Created Highlight element

---
+++ client/src/components/Highlight.tsx
@@ -0,0 +1,22 @@
+
+
+import type {IconType} from "react-icons";
+
+interface HighlightProps {
+    Icon: IconType;
+    text: string;
+}
+
+
+function Highlight({Icon, text}: HighlightProps) {
+    return (
+        <div className={"flex flex-col items-center w-fit"}>
+            <span className={"inline-flex w-16 h-16 bg-primary-100 rounded-full items-center justify-center text-3xl"}>
+                <Icon />
+            </span>
+            <span className={"mt-1"}>{text}</span>
+        </div>
+    )
+}
+
+export default Highlight
