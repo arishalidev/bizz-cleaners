36ef08e06e0b365de646e5c0528f4cd84d63aef3
arishalidev <hello@arishali.dev>
2026-05-15 14:44:57 -0500

    Created form to enter zip code and function to validate zip code

--- client/src/pages/PickupAndDelivery/ServiceArea.tsx
+++ client/src/pages/PickupAndDelivery/ServiceArea.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SectionLabel from "../../components/SectionLabel.tsx";
 import Headline from "../../components/Headline.tsx";
 import Body from "../../components/Body.tsx";
 
 import {APIProvider, Map, Polygon} from '@vis.gl/react-google-maps';
+import Button from "../../components/Button.tsx";
+import { MdOutlineLocationOn } from "react-icons/md";
+
 
 interface ServiceAreaProps {
     apiKey: string | undefined;
 }
 
 const ServiceArea: React.FC<ServiceAreaProps> = ({apiKey} : ServiceAreaProps) => {
+
+    const [zipCode, setZipCode] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    function validateZipCode() {
+        if(zipCode === "") {
+            setErrorMessage("Please enter a zip code")
+            return false;
+        }
+
+        if(isNaN(Number(zipCode))) {
+            setErrorMessage("Zip code must be digits only")
+            return false;
+        }
+
+        if(zipCode.length < 5) {
+            setErrorMessage("Zip code is too short")
+            return false;
+        }
+
+        if(zipCode.length > 5) {
+            setErrorMessage("Zip code is too long")
+            return false;
+        }
+
+        setErrorMessage("");
+        return true;
+    }
+
 
     const position = {lat: 33.01602299, lng: -97.07124099};
 
@@ -19,9 +51,9 @@ const ServiceArea: React.FC<ServiceAreaProps> = ({apiKey} : ServiceAreaProps) =>
             <Headline className={"mt-4"}>Are we in your neighborhood?</Headline>
             <Body className={"mt-2"}>We've called North DFW home for six years, and our routes keep growing. Pop in your zip code or share your location to see if we're already coming your way — and if we're not yet, we'd love to know you're waiting.</Body>
 
-            {apiKey && <APIProvider apiKey={apiKey}>
-                <div className={"w-full h-150 mt-4"}>
-                    <Map defaultCenter={position} defaultZoom={10.7} mapId="59b74ff889c3544d96f3646f"></Map>
+            {apiKey && <APIProvider>
+                <div className={"w-full h-150 mt-6"}>
+                    <Map defaultCenter={position} defaultZoom={10.4} mapId="59b74ff889c3544d96f3646f"></Map>
 
                     <Polygon
                         paths={[
@@ -49,6 +81,23 @@ const ServiceArea: React.FC<ServiceAreaProps> = ({apiKey} : ServiceAreaProps) =>
                 </div>
             </APIProvider>}
 
+            <div className={"mt-3"}>
+                <form className={"flex gap-3 items-center"}>
+                    <input type={"text"} 
+                            inputMode={"numeric"}
+                           value={zipCode}
+                           onChange={(e) => setZipCode(e.target.value)}
+                           placeholder={"Enter your zip code"} className={"outline p-4 w-full text-lg"}/>
+                    <Button type={"button"} variant={"secondary"} className={"outline-1 w-1/2"} onClick={validateZipCode}>Enter</Button>
+                </form>
+                {errorMessage && <div className={"text-red-500 mt-1"}>{errorMessage}</div>}
+
+                <button className={"mt-2 inline-flex items-center gap-1 text-primary-600 font-semibold relative text-[18px]/8"}>
+                     Use My Location<MdOutlineLocationOn className={"text-xl"}/>
+                    <div className={"absolute h-1 bg-purple-200 w-full bottom-1"}></div>
+                </button>
+
+            </div>
 
         </div>
     );
