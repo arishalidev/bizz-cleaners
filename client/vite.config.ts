b5444751d37caa40ed21a6e0b85f2b190c2d5109
arishalidev <hello@arishali.dev>
2026-05-25 08:06:31 -0500

    added allowed host to vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
+    allowedHosts: ["bizz-cleaners-production.up.railway.app"],
     port: parseInt(process.env.PORT || '5173'),
   }
 });
