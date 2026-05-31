8ac581b95cb17ddb012fa968fcc88456fa6f43a1
arishalidev <hello@arishali.dev>
2026-05-31 09:52:05 -0500

    highlights are now clickable buttons

--- client/src/pages/Home/Highlight.tsx
+++ client/src/pages/Home/Highlight.tsx
@@ -1,20 +1,26 @@
 import type {IconType} from "react-icons";
 import Body from "../../components/Body.tsx";
+import React from "react";
 
 interface HighlightProps {
     Icon: IconType,
     text: string,
+    highlightId: number
+    setModal: React.Dispatch<React.SetStateAction<number | undefined>>;
+
 }
 
 
-function Highlight({Icon, text}: HighlightProps) {
+function Highlight({Icon, text, highlightId, setModal}: HighlightProps) {
     return (
-        <div className={`flex flex-col w-fit items-center min-[850px]:flex-1`}>
+        <button className={`group flex flex-col w-fit items-center min-[850px]:flex-1`} onClick={() => setModal(highlightId)}>
             <span className={"inline-flex w-16 h-16 md:w-20 md:h-20 bg-primary-100 rounded-full items-center justify-center text-3xl"}>
-                <Icon className={"md:text-4xl"}/>
+                <span className={"inline-flex w-15 h-15 md:w-18 md:h-18 group-hover:bg-primary-200 rounded-full items-center justify-center text-3xl"}>
+                    <Icon className={"md:text-4xl"}/>
+                </span>
             </span>
             <Body className={`mt-2 font-semibold text-primary-500`}>{text}</Body>
-        </div>
+        </button>
     )
 }
 
--- client/src/pages/Home/Highlights.tsx
+++ client/src/pages/Home/Highlights.tsx
@@ -4,22 +4,41 @@ import { PiDoor } from "react-icons/pi";
 import { PiStar } from "react-icons/pi";
 
 import Highlight from "./Highlight.tsx";
+import {useState} from "react";
 
 
 const highlights = [
-    {icon: PiCoatHanger, text: "In House Cleaning", key: 1},
-    {icon: PiClock, text: "Same Day Services", key: 2},
-    {icon: PiDoor, text: "24/7 Drop Off", key: 3},
-    {icon: PiStar, text: "Earn Reward Points", key: 4}
+    {icon: PiClock, text: "Same Day Services", key: 1},
+    {icon: PiDoor, text: "24/7 Drop Off", key: 2},
+    {icon: PiStar, text: "Earn Reward Points", key: 3},
+    {icon: PiCoatHanger, text: "In House Cleaning", key: 4},
 ]
 function Highlights() {
+
+    const [modal, setModal] = useState<number | undefined>(undefined)
+
+    const modalNum = highlights.find(highlight => highlight.key === modal)
+
     return (
         <div className={"responsive-py px-2 max-w-5xl mx-auto"}>
             <div className={"grid grid-cols-2 justify-center place-items-center gap-y-10 md:gap-y-14 gap-x-2 min-[850px]:flex"}>
                 {highlights.map(highlight =>
-                    <Highlight Icon={highlight.icon} text={highlight.text} key={highlight.key}/>
+                    <Highlight Icon={highlight.icon} text={highlight.text} key={highlight.key} highlightId={highlight.key} setModal={setModal}/>
                 )}
             </div>
+
+            {modal &&
+                <div className={"w-4/5 md:w-3/4 min-[850px]:w-5/6 mx-auto h-48 mt-12 bg-primary-50 rounded-md shadow-md"}>
+                    {modalNum &&
+                        <div>
+                            <modalNum.icon></modalNum.icon>
+                            {modalNum.text}
+                        </div>
+
+                    }
+                    <button className={"ml-auto"} onClick={() => setModal(undefined)}>close</button>
+                </div>
+            }
         </div>
     )
 }
