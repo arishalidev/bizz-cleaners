33e847d28fa3485e622aa7052314dc1d5b8f9c56
arishalidev <hello@arishali.dev>
2026-06-02 17:47:41 -0500

    Highlights now display information about each service

--- client/src/pages/Home/Highlight.tsx
+++ client/src/pages/Home/Highlight.tsx
@@ -14,9 +14,9 @@ interface HighlightProps {
 function Highlight({Icon, text, highlightId, setModal}: HighlightProps) {
     return (
         <button className={`group flex flex-col w-fit items-center min-[850px]:flex-1`} onClick={() => setModal(highlightId)}>
-            <span className={"inline-flex w-16 h-16 md:w-20 md:h-20 bg-primary-100 rounded-full items-center justify-center text-3xl"}>
-                <span className={"inline-flex w-15 h-15 md:w-18 md:h-18 group-hover:bg-primary-200 rounded-full items-center justify-center text-3xl"}>
-                    <Icon className={"md:text-4xl"}/>
+            <span className={"inline-flex size-16 md:size-20 bg-primary-100 rounded-full items-center justify-center text-3xl"}>
+                <span className={"inline-flex size-15 md:size-18 group-hover:bg-primary-200 rounded-full items-center justify-center text-3xl md:text-4xl"}>
+                    <Icon/>
                 </span>
             </span>
             <Body className={`mt-2 font-semibold text-primary-500`}>{text}</Body>
--- client/src/pages/Home/Highlights.tsx
+++ client/src/pages/Home/Highlights.tsx
@@ -9,7 +9,7 @@ import {useState} from "react";
 
 const highlights = [
     {icon: PiClock, text: "Same Day Services", key: 1},
-    {icon: PiDoor, text: "24/7 Drop Off", key: 2},
+    {icon: PiDoor, text: "24/7 Drop Off", longText: "Drop off clothes anytime with our drop off box. Simply put your labeled bag in the box, and we will clean it at our earliest convince ..", key: 2},
     {icon: PiStar, text: "Earn Reward Points", key: 3},
     {icon: PiCoatHanger, text: "In House Cleaning", key: 4},
 ]
@@ -28,15 +28,23 @@ function Highlights() {
             </div>
 
             {modal &&
-                <div className={"w-4/5 md:w-3/4 min-[850px]:w-5/6 mx-auto h-48 mt-12 bg-primary-50 rounded-md shadow-md"}>
-                    {modalNum &&
-                        <div>
-                            <modalNum.icon></modalNum.icon>
-                            {modalNum.text}
+                <div className={"w-md md:w-xl min-[850px]:w-2xl mx-auto h-fit mt-12 bg-primary-50 rounded-md shadow-md"}>
+                    <div className={"px-8 py-4 flex gap-18 min-[850px]:gap-36"}>
+                        {modalNum &&
+                        <div className={"flex flex-col gap-2"}>
+                            <div
+                                className={"text-3xl md:text-4xl flex justify-center items-center size-15 md:size-18 rounded-full bg-primary-100"}>
+                                <modalNum.icon></modalNum.icon>
+                            </div>
+                            <div className={"font-semibold "}>{modalNum.text}</div>
+                            <div className={"mt-2"}>{modalNum.longText}</div>
                         </div>
 
                     }
-                    <button className={"ml-auto"} onClick={() => setModal(undefined)}>close</button>
+                        <div className={"ml-auto"}>
+                            <button className={"bg-primary-200 p-2 rounded-md"} onClick={() => setModal(undefined)}>close</button>
+                        </div>
+                    </div>
                 </div>
             }
         </div>
