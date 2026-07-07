1b5363dea6fd9df1435bb7e13b12374c669711ea
arishalidev <hello@arishali.dev>
2026-07-07 08:14:34 -0500

    Updated hero for #2

--- client/src/pages/Home/Hero.tsx
+++ client/src/pages/Home/Hero.tsx
@@ -1,7 +1,6 @@
 import '../../App.css';
-import heroImage from '../../assets/hero-image.jpg';
+import heroImage from '../../assets/hero-image.png';
 import Button from "../../components/Button.tsx";
-import Body from "../../components/Body.tsx";
 import { linkToDirections, linkToPortal } from "../../utils/links.ts";
 import {useContext, useEffect, useRef} from "react";
 import {NavbarContext} from "../../contexts/NavbarContext.tsx";
@@ -30,22 +29,16 @@ function Hero() {
         <div className={"relative"} ref={heroRef}>
 
             <img src={heroImage} alt={"Clean pressed shirts"} className={"block w-full object-cover min-h-105 md:max-h-160"}/>
-            <div className={"absolute inset-0 bg-[hsla(0,0%,0%,.7)]"}></div>
+            <div className={"absolute inset-0 bg-[hsla(0,0%,0%,.15)]"}></div>
 
             <div className={"responsive-px limit-size"}>
-                <div className={"absolute top-24 min-[850px]:top-28 text-white space-y-2"}>
-                    <h1 className={"text-3xl font-semibold md:text-5xl md:mb-5"}>Professional garment care,<br className={"max-[440px]:hidden"}/>  delivered to your door</h1>
-                    <h2 className={"text-xl font-normal md:text-2xl"}>Proudly serving Flower Mound, Highland Village,<br className={"max-[500px]:hidden"}/> Lewisville, and surrounding communities</h2>
+                <div className={"absolute top-24 lg:top-30 text-white space-y-2"}>
+                    <h1 className={"text-3xl text-shadow-lg font-bold md:text-4xl lg:text-5xl lg:mb-5"}>Professional garment care,<br className={"max-[440px]:hidden"}/>  delivered to your door</h1>
+                    <h2 className={"text-xl lg:text-2xl text-shadow-lg font-normal"}>Proudly serving Flower Mound,<br className={"max-[500px]:hidden"}/> Highland Village, Lewisville,<br className={"max-[500px]:hidden"}/> and surrounding communities</h2>
 
-                    <div className={"mt-12 flex space-x-4 md:space-x-6"}>
+                    <div className={"mt-12 lg:mt-16 flex space-x-4 md:space-x-6"}>
                         <Button variant={"primary"} onClick={linkToPortal}>Schedule Pickup</Button>
                         <Button variant={"secondary"} onClick={linkToDirections}>Get Directions</Button>
-                    </div>
-
-                    <div className={"mt-4 md:mt-6"}>
-                        <Body>
-                            Or call us at <u><a href={"tel:+19723551807"}>+1 972-355-1807</a></u>
-                        </Body>
                     </div>
                 </div>
             </div>
