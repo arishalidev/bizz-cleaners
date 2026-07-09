65fc14d0b4d9cee6ca529569493c91b6666f5b9d
arishalidev <hello@arishali.dev>
2026-07-09 08:52:18 -0500

    added image highlights for #3

--- client/src/pages/Home/Highlight.tsx
+++ client/src/pages/Home/Highlight.tsx
@@ -3,25 +3,29 @@ import Body from "../../components/Body.tsx";
 
 interface Highlight {
     Icon: IconType,
+    image?: string,
     text: string,
     description: string,
 }
 
 
-function Highlight({Icon, text, description}: Highlight) {
+function Highlight({Icon, text, description, image}: Highlight) {
     return (
         <div>
-
-            <div className={"grid grid-cols-[auto_1fr] gap-x-4"}>
-                <div className={`flex w-fit gap-2 items-baseline-last`}>
-                <span
-                    className={"inline-flex w-14 h-14 bg-primary-100 rounded-full items-center justify-center text-3xl"}>
+            <div className={"flex gap-x-4"}>
+                <div className={"mt-2"}>
+                <span className={"inline-flex w-14 h-14 bg-primary-100 rounded-full items-center justify-center text-3xl"}>
                     <Icon className={"text-3xl"}/>
                 </span>
                 </div>
 
-                    <Body className={`font-semibold content-center`}>{text}</Body>
+                <div>
+                    <Body className={`font-semibold`}>{text}</Body>
                     <Body className={"col-start-2"}>{description}</Body>
+                    {image && <img src={image} alt={"Drop off box located at the front of our store"} className={"w-full mt-1"}/>
+                    }
+
+                </div>
             </div>
 
         </div>
--- client/src/pages/Home/Highlights.tsx
+++ client/src/pages/Home/Highlights.tsx
@@ -1,5 +1,8 @@
 import { PiClock } from "react-icons/pi";
-import { PiStar } from "react-icons/pi";
+import { PiLeaf } from "react-icons/pi";
+import { PiDoor } from "react-icons/pi";
+import { PiVan } from "react-icons/pi";
+
 import dropOffBox from "../../assets/dropOffBox.png"
 import vanAtFrontDoor from "../../assets/vanAtFrontDoor.png"
 
@@ -7,18 +10,19 @@ import Highlight from "./Highlight.tsx";
 
 
 const sameDayHighlight = {icon: PiClock, text: "Same-day service", description: "Drop off qualifying items before 9:00 AM and have them ready by 3:00 PM."}
-const ecoFriendlyHighlight = {icon: PiStar, text: "Eco-friendly cleaning", description: "We use modern hydrocarbon cleaning technology instead of harsh perc-based chemicals."}
-
-const deliveryHighlight = {image: vanAtFrontDoor, text: "Convenient pickup & delivery", description: "Professional laundry and dry cleaning services without adding another errand to your week.", key: 2}
-const dropOffBoxHighlight = {image: dropOffBox, text: "24/7 Secure drop-off", description: "Use our secure drop box whenever it fits your schedule.", key: 3}
+const ecoFriendlyHighlight = {icon: PiLeaf, text: "Eco-friendly cleaning", description: "We use modern hydrocarbon cleaning technology instead of harsh perc-based chemicals."}
+const deliveryHighlight = {image: vanAtFrontDoor, icon: PiVan, text: "Convenient pickup & delivery", description: "Professional laundry and dry cleaning services without adding another errand to your week.", key: 2}
+const dropOffBoxHighlight = {image: dropOffBox, icon: PiDoor, text: "24/7 Secure drop-off", description: "Use our secure drop box whenever it fits your schedule.", key: 3}
 
 function Highlights() {
 
     return (
         <div className={"responsive-py px-2 max-w-5xl mx-auto"}>
-            <div className={"flex flex-col justify-center place-items-center gap-4"}>
+            <div className={"flex flex-col justify-center place-items-center gap-6"}>
                 <Highlight Icon={sameDayHighlight.icon} text={sameDayHighlight.text} description={sameDayHighlight.description}/>
+                <Highlight Icon={dropOffBoxHighlight.icon} image={dropOffBoxHighlight.image} text={dropOffBoxHighlight.text} description={dropOffBoxHighlight.description}/>
                 <Highlight Icon={ecoFriendlyHighlight.icon} text={ecoFriendlyHighlight.text} description={ecoFriendlyHighlight.description}/>
+                <Highlight Icon={deliveryHighlight.icon} image={deliveryHighlight.image} text={deliveryHighlight.text} description={deliveryHighlight.description}/>
             </div>
         </div>
     )
