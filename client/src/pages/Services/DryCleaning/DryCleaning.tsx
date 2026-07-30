7b453cbb05510d4af0b07ea3f9842ed0fa908aa6
arishalidev <hello@arishali.dev>
2026-07-30 15:10:53 -0500

    Added busines info section to dry cleaning page

--- client/src/pages/Services/DryCleaning/DryCleaning.tsx
+++ client/src/pages/Services/DryCleaning/DryCleaning.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SecondaryHero from "../../../components/SecondaryHero.tsx";
 import Introduction from "./Introduction.tsx";
 import Highlights from "./Highlights.tsx";
 import CleanerApproach from "./CleanerApproach.tsx";
 import TurnaroundTimes from "./TurnaroundTimes.tsx";
 import FAQSection from "./FAQSection.tsx"
+import {apiBase} from "../../../utils/links.ts";
+import BusinessInfo from "./BusinessInfo.tsx";
 
 interface DryCleaningProps {
 
 }
 
 const DryCleaning: React.FC<DryCleaningProps> = ({}) => {
+
+    const [hoursOfOperation, setHoursOfOperation] = useState<string[]>([]);
+    const [isOpen, setIsOpen] = useState<boolean | undefined>();
+
+    useEffect(() => {
+        fetch(`${apiBase}/api/get/business-information`)
+            .then(res => res.json())
+            .then(data => {
+                setHoursOfOperation(data.hoursOfOperation);
+                setIsOpen(data.isOpen);
+            })
+    }, []);
+
     return (
         <div>
             <SecondaryHero header={"Dry cleaning done with care"} smallSize={true}/>
@@ -19,6 +34,8 @@ const DryCleaning: React.FC<DryCleaningProps> = ({}) => {
             <CleanerApproach/>
             <TurnaroundTimes/>
             <FAQSection/>
+            <BusinessInfo hoursOfOperation={hoursOfOperation} isOpen={isOpen}/>
+
         </div>
     );
 };
