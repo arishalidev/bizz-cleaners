5ece161a3c0f24b57f8bd15b459f99161e334e48
arishalidev <hello@arishali.dev>
2026-08-03 10:24:02 -0500

    removed extra photo from wash and fold section

--- client/src/pages/Services/WashAndFold/BusinessInfo.tsx
+++ client/src/pages/Services/WashAndFold/BusinessInfo.tsx
@@ -6,7 +6,6 @@ import {linkToDirections, linkToPortal} from "../../../utils/links.ts";
 import HoursOfOperation from "../../../components/hoursOfOperation.tsx";
 import ContactInfo from "../../../components/ContactInfo.tsx";
 import Body from "../../../components/Body.tsx";
-import blousePressed from "../../../assets/blousePressed.jpg";
 
 
 interface BusinessInfoProps {
@@ -52,9 +51,6 @@ const BusinessInfo: React.FC<BusinessInfoProps> = ({ hoursOfOperation, isOpen } 
                                 className={"h-96 md:h-112 w-full"}/>
                         </div>
                     </div>
-
-                    <img className={"rounded-md mx-auto max-w-5xl w-full"} src={blousePressed}
-                         alt={"Blouse being pressed"}/>
 
                     <div className={"mt-8 lg:mt-12 max-w-140 flex flex-col gap-2 mb-6"}>
                         <Headline>Ready to skip laundry day?</Headline>
